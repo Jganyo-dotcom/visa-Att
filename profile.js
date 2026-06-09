dcc088fe5f82cc1aa8a333ad20ebf367dc57ab5e
James Ganyo <elikemejay@gmail.com>
2026-06-09 16:34:38 +0000

    avatar image

--- profile.js
+++ profile.js
@@ -1,6 +1,6 @@
 const baseApi = "https://attandance-app-1.onrender.com/";
-const DEFAULT_AVATAR = "http://googleusercontent.com/image_collection/image_retrieval/2446120568034360762_0";
-
+// Update this constant at the very top of profile.js
+const DEFAULT_AVATAR = "data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='%2394a3b8'><path d='M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z'/></svg>";
 const token = localStorage.getItem("token");
 const user = JSON.parse(localStorage.getItem("user"));
 
