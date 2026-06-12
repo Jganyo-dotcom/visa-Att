5a55d2985b40c74b71ac80dd2e02e73f325b5949
James Ganyo <elikemejay@gmail.com>
2026-06-12 15:22:56 +0000

    COMMENTED OUT

--- everyone/everyone.js
+++ everyone/everyone.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://127.0.0.1:4444/api/guest";
+//const API_BASE_URL = "http://127.0.0.1:4444/api/guest";
 const API_BASE_URL = "https://attandance-app-1.onrender.com/api/guest";
 
 // Syncing state variables directly with your backend response properties
