f2200e9b070b48fd70d4a50d6d6880e8d3037734
James Ganyo <elikemejay@gmail.com>
2026-06-16 08:53:27 +0000

    messages hub

--- global-sidebar.js
+++ global-sidebar.js
@@ -12,14 +12,12 @@ document.addEventListener("DOMContentLoaded", () => {
     <button id="sessionM" class="btn-secondary">⏳ Manage Sessions</button>
     <button id="MA" class="btn-secondary">📝 Mark Attendance</button>
     <button id="approveAttendance" class="btn-secondary">📝Pending Attendance</button>
-    <button id="code" class="btn-secondary">📷 Mark Attendance By Scan</button>
     <button id="qrCodePage" class="btn-secondary">📷 Generage code</button>
     <button id="DoubleServicePage" class="btn-secondary">🔄 Double Pip</button>
     <button id="tend" class="btn-secondary">ℹ️ Attendance Info</button>
     <button id="followPage" class="btn-secondary">🔍 Filter Attendance</button>
     <button id="analysisPage" class="btn-secondary">📈 Analysis Of Attendance</button>
     <button id="peoplePage" class="btn-secondary">🚫 Manage Absentees</button>
-    <button id="pendingA" class="btn-secondary">🔑Pending Approvals</button>
     <button id="database" class="btn-secondary">🗄️ Database Records</button>
     <button id="profilePage" class="btn-secondary">⚙️ Settings & Profile</button>
     <button id="signOutBtn" class="btn-signout">🚪 Sign Out</button>
